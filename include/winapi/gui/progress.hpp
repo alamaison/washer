64e46b443b261a19261bee6cf0be62286c220741
Alexander Lamaison <awl03@doc.ic.ac.uk>
2012-04-29 15:12:29 +0100

    Added IProgressDialog wrapper.

---
+++ include/winapi/gui/progress.hpp
@@ -0,0 +1,353 @@
+/**
+    @file
+
+    IProgressDialog C++ wrapper.
+
+    @if license
+
+    Copyright (C) 2012  Alexander Lamaison <awl03@doc.ic.ac.uk>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    If you modify this Program, or any covered work, by linking or
+    combining it with the OpenSSL project's OpenSSL library (or a
+    modified version of that library), containing parts covered by the
+    terms of the OpenSSL or SSLeay licenses, the licensors of this
+    Program grant you additional permission to convey the resulting work.
+
+    @endif
+*/
+
+#ifndef WINAPI_GUI_PROGRESS_HPP
+#define WINAPI_GUI_PROGRESS_HPP
+#pragma once
+
+#include <boost/detail/scoped_enum_emulation.hpp> // BOOST_SCOPED_ENUM
+#include <boost/exception/errinfo_api_function.hpp> // errinfo_api_function
+#include <boost/exception/info.hpp> // errinfo
+#include <boost/filesystem/path.hpp> // wpath
+#include <boost/noncopyable.hpp> // noncopyable
+#include <boost/throw_exception.hpp> // BOOST_THROW_EXCEPTION
+
+#include <comet/error.h>
+#include <comet/ptr.h> // com_ptr
+
+#include <cassert> // assert
+#include <stdexcept> // invalid_argument
+#include <string>
+
+#include <ShlObj.h> // IProgressDialog
+
+template<> struct comet::comtype<IProgressDialog>
+{
+	static const IID& uuid() throw() { return IID_IProgressDialog; }
+	typedef IUnknown base;
+};
+
+namespace winapi {
+namespace gui {
+
+/**
+ * Progress dialogue window.
+ *
+ * This class displays the window when the object is created and hides it
+ * when this object is destroyed.
+ *
+ * @todo  Can the caller change the title after creation (display)
+ *
+ * SetAnimation is not supported because Windows Vista and later don't make use
+ * of it so it's not worth the effort.
+ */
+class progress : boost::noncopyable
+{
+public:
+
+	BOOST_SCOPED_ENUM_START(modality)
+	{
+		modal,
+		non_modal
+	};
+	BOOST_SCOPED_ENUM_END;
+
+	BOOST_SCOPED_ENUM_START(time_estimation)
+	{
+		automatic_time_estimate,
+		none
+	};
+	BOOST_SCOPED_ENUM_END;
+
+	BOOST_SCOPED_ENUM_START(bar_type)
+	{
+		progress,
+		marquee,
+		none
+	};
+	BOOST_SCOPED_ENUM_END;
+
+	BOOST_SCOPED_ENUM_START(minimisable)
+	{
+		yes,
+		no
+	};
+	BOOST_SCOPED_ENUM_END;
+
+	BOOST_SCOPED_ENUM_START(cancellability)
+	{
+		cancellable,
+		uncancellable
+	};
+	BOOST_SCOPED_ENUM_END;
+
+	/**
+	 * Create and display the default system progress dialogue.
+	 *
+	 * It will be hidden and destroyed when this object is destroyed.
+	 *
+	 * @param hwnd_parent      Parent window.
+	 * @param title            Text for the progress window's title bar.
+	 * @param modality         Modal relationship of dialogue to its parent
+	 *                         window.
+	 * @param time_estimation  Whether dialogue automatically estimates
+	 *                         remaining time.
+	 * @param bar_type         Style of progress bar.
+	 * @param minimisable      Whether dialogue can me minimised.
+	 * @param cancellability   Whether dialogue allows user to cancel the
+	 *                         operation.
+	 * @param ole_site         OLE site.  Something to do with modal behaviour.
+	 */
+	progress(
+		HWND hwnd_parent, const std::wstring& title,
+		BOOST_SCOPED_ENUM(modality) modality,
+		BOOST_SCOPED_ENUM(time_estimation) time_estimation,
+		BOOST_SCOPED_ENUM(bar_type) bar_type,
+		BOOST_SCOPED_ENUM(minimisable) minimisable,
+		BOOST_SCOPED_ENUM(cancellability) cancellability,
+		comet::com_ptr<IUnknown> ole_site=NULL)
+		: m_progress(comet::com_ptr<IProgressDialog>(CLSID_ProgressDialog))
+	{
+		_init(
+			hwnd_parent, title, modality, time_estimation, bar_type,
+			minimisable, cancellability, ole_site);
+	}
+
+	/**
+	 * Display the given progress dialogue for the lifetime of this object.
+	 *
+	 * @param progress         The existing IProgressDialog.  This object will 
+	 *                         display the dialogue and hide it when the object
+	 *                         is destroyed.
+	 * @param hwnd_parent      Parent window.
+	 * @param title            Text for the progress window's title bar.
+	 * @param modality         Modal relationship of dialogue to its parent
+	 *                         window.
+	 * @param time_estimation  Whether dialogue automatically estimates
+	 *                         remaining time.
+	 * @param bar_type         Style of progress bar.
+	 * @param minimisable      Whether dialogue can me minimised.
+	 * @param cancellability   Whether dialogue allows user to cancel the
+	 *                         operation.
+	 * @param ole_site         OLE site.  Something to do with modal behaviour.
+	 *
+	 * @todo  What happens if the dialog is already started?
+	 */
+	progress(
+		comet::com_ptr<IProgressDialog> progress, HWND hwnd_parent,
+		const std::wstring& title,
+		BOOST_SCOPED_ENUM(modality) modality,
+		BOOST_SCOPED_ENUM(time_estimation) time_estimation,
+		BOOST_SCOPED_ENUM(bar_type) bar_type,
+		BOOST_SCOPED_ENUM(minimisable) minimisable,
+		BOOST_SCOPED_ENUM(cancellability) cancellability,
+		comet::com_ptr<IUnknown> ole_site=NULL)
+		: m_progress(progress)
+	{
+		if (!progress)
+			BOOST_THROW_EXCEPTION(
+				std::invalid_argument("Progress dialog required"));
+
+		_init(
+			hwnd_parent, title, modality, time_estimation, bar_type,
+			minimisable, cancellability, ole_site);
+	}
+
+	~progress()
+	{
+		m_progress->StopProgressDialog();
+	}
+
+	/**
+	 * Set the indexth line of the display to the given text.
+	 *
+	 * Setting a third line will throw an exception if automatic time estimation
+	 * is enabled.  The third line is used to display that estimate.
+	 */
+	void line(DWORD index, const std::wstring& text)
+	{
+		HRESULT hr = m_progress->SetLine(index, text.c_str(), FALSE, NULL);
+		if (FAILED(hr))
+			BOOST_THROW_EXCEPTION(
+				com_error_from_interface(m_progress, hr));
+	}
+
+	/**
+	 * Set the indexth line of the display to the given path but compress it.
+	 *
+	 * The path must be a Windows filesystem path and will only be compressed
+	 * if too wide to fit in the dialogue window.
+	 *
+	 * Setting a third line will throw an exception if automatic time estimation
+	 * is enabled.  The third line is used to display that estimate.
+	 */
+	void line_as_compressable_path(
+		DWORD index, const boost::filesystem::wpath& path)
+	{
+		HRESULT hr = m_progress->SetLine(
+			index, path.file_string().c_str(), TRUE, NULL);
+		if (FAILED(hr))
+			BOOST_THROW_EXCEPTION(
+				com_error_from_interface(m_progress, hr));
+	}
+
+	/**
+	 * Update the indicator to show current progress level.
+	 */
+	void update(ULONGLONG so_far, ULONGLONG out_of)
+	{
+		HRESULT hr = m_progress->SetProgress64(so_far, out_of);
+		if (FAILED(hr))
+			BOOST_THROW_EXCEPTION(
+				com_error_from_interface(m_progress, hr));
+	}
+
+	/**
+	 * Has the user cancelled the operation via the progress dialogue?
+	 */
+	bool user_cancelled() const
+	{
+		return m_progress->HasUserCancelled() != FALSE;
+	}
+
+private:
+
+	/**
+	 * Common constructor.
+	 */
+	void _init(
+		HWND hwnd_parent, const std::wstring& title,
+		BOOST_SCOPED_ENUM(modality) modality,
+		BOOST_SCOPED_ENUM(time_estimation) time_estimation,
+		BOOST_SCOPED_ENUM(bar_type) bar_type,
+		BOOST_SCOPED_ENUM(minimisable) minimisable,
+		BOOST_SCOPED_ENUM(cancellability) cancellability,
+		comet::com_ptr<IUnknown> ole_site)
+	{
+		HRESULT hr;
+
+		hr = m_progress->SetTitle(title.c_str());
+		if (FAILED(hr))
+			BOOST_THROW_EXCEPTION(
+				com_error_from_interface(m_progress, hr));
+
+		hr = m_progress->StartProgressDialog(
+			hwnd_parent, ole_site.get(),
+			_options_to_progress_flags(
+				modality, time_estimation, bar_type, minimisable,
+				cancellability),
+			NULL);
+		if (FAILED(hr))
+			BOOST_THROW_EXCEPTION(
+				com_error_from_interface(m_progress, hr));
+	}
+
+	DWORD _options_to_progress_flags(
+		BOOST_SCOPED_ENUM(modality) modality,
+		BOOST_SCOPED_ENUM(time_estimation) time_estimation,
+		BOOST_SCOPED_ENUM(bar_type) bar_type,
+		BOOST_SCOPED_ENUM(minimisable) minimisable,
+		BOOST_SCOPED_ENUM(cancellability) cancellability)
+	{
+		DWORD flags = PROGDLG_NORMAL;
+
+		switch (modality)
+		{
+		case modality::modal:
+			flags |= PROGDLG_MODAL;
+			break;
+		case modality::non_modal:
+			break;
+		default:
+            BOOST_THROW_EXCEPTION(
+                std::invalid_argument("Unknown modality"));
+		}
+
+		switch (time_estimation)
+		{
+		case time_estimation::automatic_time_estimate:
+			flags |= PROGDLG_AUTOTIME;
+			break;
+		case time_estimation::none:
+			break;
+		default:
+			BOOST_THROW_EXCEPTION(
+				std::invalid_argument("Unknown time estimation"));
+		}
+
+		switch (bar_type)
+		{
+		case bar_type::progress:
+			break;
+		case bar_type::marquee:
+			flags |= PROGDLG_MARQUEEPROGRESS;
+			break;
+		case bar_type::none:
+			flags |= PROGDLG_NOPROGRESSBAR;
+			break;
+		default:
+			BOOST_THROW_EXCEPTION(
+				std::invalid_argument("Unknown bar type"));
+		}
+
+		switch (minimisable)
+		{
+		case minimisable::yes:
+			break;
+		case minimisable::no:
+			flags |= PROGDLG_NOMINIMIZE;
+			break;
+		default:
+			BOOST_THROW_EXCEPTION(
+				std::invalid_argument("Unknown minimisability"));
+		}
+
+		switch (cancellability)
+		{
+		case cancellability::cancellable:
+			break;
+		case cancellability::uncancellable:
+			flags |= PROGDLG_NOCANCEL;
+			break;
+		default:
+			BOOST_THROW_EXCEPTION(
+				std::invalid_argument("Unknown cancellability"));
+		}
+
+		return flags;
+	}
+
+	comet::com_ptr<IProgressDialog> m_progress;
+};
+
+}} // namespace winapi::gui
+
+#endif
---
+++ test/progress_test.cpp
@@ -0,0 +1,156 @@
+/**
+    @file
+
+    Tests for IProgressDialog wrapper.
+
+    @if license
+
+    Copyright (C) 2012  Alexander Lamaison <awl03@doc.ic.ac.uk>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    If you modify this Program, or any covered work, by linking or
+    combining it with the OpenSSL project's OpenSSL library (or a
+    modified version of that library), containing parts covered by the
+    terms of the OpenSSL or SSLeay licenses, the licensors of this
+    Program grant you additional permission to convey the resulting work.
+
+    @endif
+*/
+
+#include <winapi/gui/progress.hpp> // test subject
+
+#include <comet/util.h> // auto_coinit
+
+#include <boost/test/unit_test.hpp>
+
+using winapi::gui::progress;
+
+using comet::auto_coinit;
+
+namespace {
+
+	class com_fixture
+	{
+	private:
+		auto_coinit com;
+	};
+
+	//const int SLEEP_TIME = 5000;
+	const int SLEEP_TIME = 5;
+
+}
+
+BOOST_FIXTURE_TEST_SUITE(progress_tests, com_fixture)
+
+/**
+ * Progress dialogue text display.
+ */
+BOOST_AUTO_TEST_CASE( text )
+{
+    progress dialogue(
+		NULL, L"Test title", progress::modality::modal, 
+		progress::time_estimation::none, progress::bar_type::progress,
+		progress::minimisable::yes, progress::cancellability::cancellable);
+	dialogue.line(1, L"Text 1");
+	dialogue.line(2, L"Text 2");
+	dialogue.line(3, L"Text 3");
+	for (int i = 0; i <= SLEEP_TIME; ++i)
+	{
+		dialogue.update(i, SLEEP_TIME);
+		::Sleep(1);
+	}
+}
+
+/**
+ * Progress dialogue long path display.
+ */
+BOOST_AUTO_TEST_CASE( path )
+{
+    progress dialogue(
+		NULL, L"Test title", progress::modality::modal, 
+		progress::time_estimation::none, progress::bar_type::progress,
+		progress::minimisable::yes, progress::cancellability::cancellable);
+	dialogue.line(1, L"Text 1");
+	dialogue.line_as_compressable_path(
+		2, L"C:\\a\\very\\extremely\\indubitably\\"
+		L"supercalifragilistically\\expialidociously\\long\\path");
+	dialogue.line_as_compressable_path(
+		3, L"/a/very/very/extremely/indubitably/"
+		L"supercalifragilistically/expialidociously/long/unix/path");
+	for (int i = 0; i <= SLEEP_TIME; ++i)
+	{
+		dialogue.update(i, SLEEP_TIME);
+		::Sleep(1);
+	}
+}
+
+/**
+ * Progress dialogue with automatic time estimation.
+ */
+BOOST_AUTO_TEST_CASE( text_with_time )
+{
+    progress dialogue(
+		NULL, L"Test title", progress::modality::modal, 
+		progress::time_estimation::automatic_time_estimate,
+		progress::bar_type::progress,
+		progress::minimisable::yes, progress::cancellability::cancellable);
+	dialogue.line(1, L"Text 1");
+	dialogue.line(2, L"Text 2");
+	for (int i = 0; i <= SLEEP_TIME; ++i)
+	{
+		dialogue.update(i, SLEEP_TIME);
+		::Sleep(3);
+	}
+}
+
+/**
+ * Progress dialogue with time and marquee.
+ */
+BOOST_AUTO_TEST_CASE( text_with_time_and_marquee )
+{
+    progress dialogue(
+		NULL, L"Test title", progress::modality::modal, 
+		progress::time_estimation::automatic_time_estimate,
+		progress::bar_type::marquee,
+		progress::minimisable::yes, progress::cancellability::cancellable);
+	dialogue.line(1, L"Text 1");
+	dialogue.line(2, L"Text 2");
+	for (int i = 0; i <= SLEEP_TIME; ++i)
+	{
+		dialogue.update(i, SLEEP_TIME);
+		::Sleep(3);
+	}
+}
+
+/**
+ * Progress dialogue with time but no progress bar.
+ */
+BOOST_AUTO_TEST_CASE( text_with_time_only )
+{
+    progress dialogue(
+		NULL, L"Test title", progress::modality::modal, 
+		progress::time_estimation::automatic_time_estimate,
+		progress::bar_type::none,
+		progress::minimisable::yes, progress::cancellability::cancellable);
+	dialogue.line(1, L"Text 1");
+	dialogue.line(2, L"Text 2");
+	for (int i = 0; i <= SLEEP_TIME; ++i)
+	{
+		dialogue.update(i, SLEEP_TIME);
+		::Sleep(3);
+	}
+}
+
+BOOST_AUTO_TEST_SUITE_END();
